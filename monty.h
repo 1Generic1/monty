5d5d7ed80cc77d3892979af7673c1a88b98d56a5
eungobs <eungobs@gmail.com>
2023-08-24 13:01:45 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -6,35 +6,38 @@
 #include <stddef.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for Monty project
- */
+* struct stack_s - doubly linked list representation of a stack (or queue)
+* @n: integer
+* @next: points to the next element of the stack (or queue)
+*
+* Description: doubly linked list node structure
+* for Monty project
+*/
 
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *next;
+int n;
+struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its associated function
- */
+* struct instruction_s - opcode and its function
+* @opcode: the opcode
+* @f: function to handle the opcode
+*
+* Description: opcode and its associated function
+*/
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern instruction_t opcodes[];
 
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 int is_integer(char *str);
 int main(int argc, char *argv[]);

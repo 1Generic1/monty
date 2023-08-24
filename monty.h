29e5137742e64f157a21b3b14cccf6c5aaa349c0
kelvin <kelvin4stunt@gmail.com>
2023-08-25 00:23:01 +0100

    monty

--- monty.h
+++ monty.h
@@ -1,6 +1,5 @@
-#ifndef MONTY_Hi
+#ifndef MONTY_H
 #define MONTY_H
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -9,15 +8,16 @@
 * struct stack_s - doubly linked list representation of a stack (or queue)
 * @n: integer
 * @next: points to the next element of the stack (or queue)
-*
+* @prev : points to the prev element
 * Description: doubly linked list node structure
 * for Monty project
 */
 
 typedef struct stack_s
 {
-int n;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,13 +27,15 @@ struct stack_s *next;
 *
 * Description: opcode and its associated function
 */
+
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern instruction_t opcodes[];
+
 void pint(stack_t **stack, unsigned int line_number);
 int is_integer(char *str);
 void push(stack_t **stack, unsigned int line_number);

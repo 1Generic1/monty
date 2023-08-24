7291e18abf179605c3e63438bc92a7e70964c976
eungobs <eungobs@gmail.com>
2023-08-24 15:30:44 -0700

    main.c

--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 #include <string.h>
 #include "monty.h"
 
-extern instruction_t opcodes[];
-
 /**
 * main - Entry point for the Monty interpreter
 * @argc: Number of command-line arguments
 * @argv: Array of command-line arguments
 * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
 */
+
+extern instruction_t opcodes[];
 int main(int argc, char *argv[])
 {
 char *line = NULL;
 size_t line_buf_size = 0;
 unsigned int line_number = 0;
 char *opcode;
-int found = 0;
+int found;
 int i;
 FILE *file;
 stack_t *stack = NULL;
@@ -27,19 +27,22 @@ if (argc != 2)
 fprintf(stderr, "USAGE: monty file\n");
 return (EXIT_FAILURE);
 }
-
 file = fopen(argv[1], "r");
 if (!file)
 {
-perror("Error opening file");
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+return (EXIT_FAILURE);
+}       file = fopen(argv[1], "r");
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 return (EXIT_FAILURE);
 }
 
-while (getline(&line, &line_buf_size, file) != -1)
+while (fgets(line, line_buf_size, file) != NULL)
 {
 line_number++;
 opcode = strtok(line, " \t\n");
-found = 0;
 for (i = 0; opcodes[i].opcode != NULL; i++)
 {
 if (strcmp(opcodes[i].opcode, opcode) == 0)
@@ -54,10 +57,8 @@ if (!found)
 fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
 free(line);
 fclose(file);
-return (EXIT_FAILURE);
 }
 }
-
 free(line);
 fclose(file);
 return (EXIT_SUCCESS);

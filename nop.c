19ed51f211a8f71dd901f6af6e6c5e209bac460d
kelvin <kelvin4stunt@gmail.com>
2023-08-25 01:51:06 +0100

    monty

--- nop.c
+++ nop.c
@@ -5,7 +5,7 @@
  * @stack: Double pointer to the stack
  * @line_number: Line number being processed
  */
-void f_nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
